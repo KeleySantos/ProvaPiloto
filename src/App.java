c7811babd19e28baa279d2bf250d4ae725a5a3dc
KeleySantos <keleysantoss17@gmail.com>
2021-10-02 17:57:35 -0300

    .

--- src/App.java
+++ src/App.java
@@ -1,67 +1,93 @@
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(final String[] args) throws Exception {
 
-        Scanner ler = new Scanner(System.in);
+        final Scanner ler = new Scanner(System.in);
 
         System.out.println("Digite quantos pilotos quer cadastrar: ");
-        int tamanho = ler.nextInt();
+        final int tamanho = ler.nextInt();
 
-        Piloto[] pilotos = new Piloto[tamanho];
-
+        final Piloto piloto = new Piloto();
+    
+        final Piloto[] pilotos = new Piloto[tamanho];
+    
         for(int i = 0; i < tamanho; i++){
-
-            Piloto piloto = new Piloto();
 
             ler.nextLine();
             System.out.println("Informe o nome do piloto: ");
             piloto.setNome(ler.nextLine());
-
+    
             ler.nextLine();
             System.out.println("Informe o CPF do piloto: ");
             piloto.setCpf(ler.nextInt());
-
+    
             System.out.println("Informe a habilitação do piloto: ");
             piloto.setHabilitacao(ler.nextInt());
-
+    
             pilotos[i] = piloto;
         }
-        
-        System.out.println("Voce deseja:");
+
+        System.out.println("MENU:");
         System.out.println("1 - Ver pilotos listados");
         System.out.println("2 - Procurar piloto atraves do CPF");
+        System.out.println("3 - Expandir pilotos listados");
+        System.out.println("4 - Sair");
         System.out.println("Digite uma opção: ");
-        int opcao = ler.nextInt();
+        final int opcao = ler.nextInt();
 
-        if(opcao == 1){
+        switch(opcao){
 
-            for(int i = 0; i < pilotos.length; i++){
+            case 1:
 
-                System.out.println("O nome do piloto cadastrado é : " + pilotos[i].getNome());
-                System.out.println("O cpf do piloto cadastrado é : " + pilotos[i].getCpf());
-                System.out.println("A habilitação do piloto cadastrado é : " + pilotos[i].getHabilitacao());
-                System.out.println();
-                System.out.println("--------------------------------------------------------------");
-                System.out.println();
-            }
+                for(int i = 0; i < pilotos.length; i++){
 
-        }else if(opcao == 2){
-
-            System.out.println("Digite o CPF: ");
-            int proccpf = ler.nextInt();
-            ler.nextLine();
-
-            for(int i = 0; i < pilotos.length; i++){
-
-                if(pilotos[i].getCpf() == proccpf){
-
-                    System.out.println("O nome do piloto cadastrado é: "+ pilotos[i].getNome());
+                    System.out.println("O nome do piloto cadastrado é : " + pilotos[i].getNome());
+                    System.out.println("O cpf do piloto cadastrado é : " + pilotos[i].getCpf());
+                    System.out.println("A habilitação do piloto cadastrado é : " + pilotos[i].getHabilitacao());
+                    System.out.println();
+                    System.out.println("--------------------------------------------------------------");
+                    System.out.println();
                 }
-            }
-        }
+            break;
 
+            case 2:
+
+                System.out.println("Digite o CPF: ");
+                final int proccpf = ler.nextInt();
+                ler.nextLine();
+
+                for(int i = 0; i < pilotos.length; i++){
+
+                    if(pilotos[i].getCpf() == proccpf){
+
+                        System.out.println("O nome do piloto cadastrado é: "+ pilotos[i].getNome());
+                    }
+                }
+            break;
+
+            case 3:
+
+                System.out.println("Digite o tamanho do novo vetor: ");
+                final int newTamanho = ler.nextInt();
+
+                final Piloto [] newPilotos = new Piloto[newTamanho];
+
+                for(int i = 0; i < newPilotos.length; i++){
+
+                    newPilotos[i] = pilotos[i];
+                }
+            break;
+
+            case 4:
+                System.out.println("--------------Programa encerrado-----------------");
+                break;
+        
+            default:
+                System.out.println("DIGITE UMA OPÇÃO VÁLIDA");
+                break;
+        }
+        
         ler.close();
-       
     }
 }
